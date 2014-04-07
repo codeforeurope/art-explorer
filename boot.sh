f007b6a16846d30cc16333dc481673e3b7e355ee
Asa Calow <asacalow@gmail.com>
2014-04-07 19:21:01 +0100

    Dockerify MongoDB

--- boot.sh
+++ boot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sed s#\<env\>#$ES_PORT# config/elasticsearch.yml.example > config/elasticsearch.yml
+sed s#\<env\>#$DB_PORT# config/mongoid.yml.example > config/mongoid.yml
 bundle exec unicorn
